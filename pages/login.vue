c26d06cffec2f0d13c957e8ef1e87cc1d7e10c6e
Bruno Pedroso <bruno.pedroso@unochapeco.edu.br>
2023-07-03 14:54:59 -0300

    foi

--- pages/login.vue
+++ pages/login.vue
@@ -1,31 +1,63 @@
 <template>
-  <v-row class="d-flex justify-center">
-    <div>
-      
-    </div>
-    <v-col class="d-flex justify-center mt-15">
-      <v-card width="500">
-        <v-sheet width="300" class="mx-auto">
+  <v-container class="d-flex justify-center text-center">
     <v-form>
-      <v-text-field 
+      <img src="./admin/loja3.png" alt="" style="height: 10%;">
+      <h1 style="height: 10%;">Faça seu login </h1>
+      <v-text-field
+      style="width: 350px;"
+      solo
+      outlined 
       label="E-mail"
       >
     </v-text-field>
-      <v-text-field
-      label="senha"
-      >
-    </v-text-field>
+    <div id="app">
+      <v-app id="inspire">
+        <v-form>
+          <v-row>
+            <v-col>
+              <v-text-field
+              v-model="password"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show1 ? 'text' : 'password'"
+              name="input-10-1"
+              label="Senha"
+              counter
+              solo
+              outlined
+              @click:append="show1 = !show1"
+              ></v-text-field>
+              <v-btn class="mr-3">Entrar</v-btn>
+              <v-btn>Cadastrar </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+        <v-btn 
+        class="mt-4"
+        href="/"
+        >Voltar a página inicial</v-btn>
+  </v-app>
+</div>
     </v-form>
-  </v-sheet>
-      </v-card>
-    </v-col>
-  </v-row>
+</v-container>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      show1: false,
+      show2: true,
+      password: 'Password',
+      rules: {
+        required: value => !!value || 'Campo Obrigatório.',
+        min: v => v.length >= 8 || 'Minimo 8 caracteres',
+        emailMatch: () => ('Usuário ou senha inválidos'),
+      },
+    }
+  },
 }
+
 </script>
 
 <style>
